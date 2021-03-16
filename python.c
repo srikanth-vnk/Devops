eaaf0a33a80c0487983b6835911091fd29488835
srikanth-vnk <srikanthjonnalagadda2@gmail.com>
2021-03-16 13:31:27 +0000

    python code

---
+++ python.c
@@ -0,0 +1,5 @@
+void main(0
+{
+ printf("hello world");
+}
+
--- sample.c
+++ sample.c
@@ -7,3 +7,15 @@ void main()
 {
  printf("hello world");
 }
+
+
+void test code
+{
+ ..........Test code......
+ ..........Test code......
+
+ ..........Test code......
+
+ ..........Test code......
+
+}
